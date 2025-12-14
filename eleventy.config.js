2c3505a205f6b6e5b9237668b9782c11793ed341
Chris G <lokioatmeal@users.noreply.github.com>
2025-12-14 03:53:56 -0800

    Cleanup (#9)
    * Adding nojekyll to src to see if that deos what I think it will.
    
    * Bunch o’ slly lil’ adjustments.
    
    * still tryna fix build with GH Actions and GH Pages.
    
    ---------
    
    Co-authored-by: Chris Golaszewski <chris-golaszewski@Chris-G-MBP.local>
    Co-authored-by: Chris G <chrisg@armorclass.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,9 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
-import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import EleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(EleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy("src/assets");
 
   // eleventyConfig.addDataExtension("json", (contents) => JSON.parse(contents)); // Revisit using _data for site-wide data.
@@ -21,6 +21,7 @@ export default async function (eleventyConfig) {
       includes: "_includes",
       output: "_site"
     },
+    pathPrefix: "/ac/",
     templateFormats: ["njk", "md", "html"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk"
